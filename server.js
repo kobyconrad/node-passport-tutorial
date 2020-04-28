880d9d11cde79c8fc10f4a5c817480cbbeea3ebd
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-04-28 16:41:20 -0700

    registers users and hashes password

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
-
 const app = express();
+const bcrypt = require("bcrypt");
+
+const users = [];
 
 app.set("view-engine", "ejs");
+
+// Means we want to be able to access our req data inside of the POST methods
+app.use(express.urlencoded({ extended: false }));
 
 /// Our Routes
 app.get("/", (req, res) => {
@@ -13,11 +18,28 @@ app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
+app.post("/login", (req, res) => {});
+
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", (req, res) => {});
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect("/login");
+  } catch {
+    res.redirect("/register");
+  }
+  console.log(users);
+});
 ///
 
 app.listen(3000);
