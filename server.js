fc11675fe550fb6f13f6cba10f077f48c574bcd6
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-04-28 16:24:53 -0700

    created register form

--- server.js
+++ server.js
@@ -2,4 +2,22 @@ const express = require("express");
 
 const app = express();
 
+app.set("view-engine", "ejs");
+
+/// Our Routes
+app.get("/", (req, res) => {
+  res.render("index.ejs", { name: "Koby" });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", (req, res) => {});
+///
+
 app.listen(3000);
