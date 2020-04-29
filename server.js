f39b0a3fbcdbe02cb2c75aa4d201c22e65fa6046
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-04-28 17:10:06 -0700

    login screen completed by broken 2801

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+
+const initializePassport = require("./passport-config");
+initializePassport(passport, (email) =>
+  users.find((user) => user.email === email)
+);
 
 const users = [];
 
@@ -8,6 +20,16 @@ app.set("view-engine", "ejs");
 
 // Means we want to be able to access our req data inside of the POST methods
 app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 /// Our Routes
 app.get("/", (req, res) => {
@@ -18,7 +40,14 @@ app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
-app.post("/login", (req, res) => {});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
